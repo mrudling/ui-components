7c70685755ed9c846d46b4449e9015f690faa345
Mike <mike.rudling@amptalk.co.jp>
2024-09-06 11:55:21 +0900

    add content

---
+++ src/module.ts
@@ -0,0 +1,16 @@
+import { defineNuxtModule, createResolver } from '@nuxt/kit'
+
+// Module options TypeScript interface definition
+export interface ModuleOptions {}
+
+export default defineNuxtModule<ModuleOptions>({
+  meta: {
+    name: 'my-module',
+    configKey: 'myModule',
+  },
+  // Default configuration options of the Nuxt module
+  defaults: {},
+  setup(_options, _nuxt) {
+    const resolver = createResolver(import.meta.url)
+  },
+})
