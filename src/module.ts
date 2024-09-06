65f85c4fd9543d517608d82b6b4d8b7e2214d662
Mike <mike.rudling@amptalk.co.jp>
2024-09-06 13:29:32 +0900

    tweaks

--- src/module.ts
+++ src/module.ts
@@ -1,16 +1,23 @@
-import { defineNuxtModule, createResolver } from '@nuxt/kit'
+import { defineNuxtModule, createResolver, addComponentsDir } from '@nuxt/kit'
 
 // Module options TypeScript interface definition
 export interface ModuleOptions {}
 
 export default defineNuxtModule<ModuleOptions>({
   meta: {
-    name: 'my-module',
-    configKey: 'myModule',
+    name: '@mrudling/ui-components',
+    configKey: 'uiComponents',
   },
   // Default configuration options of the Nuxt module
   defaults: {},
-  setup(_options, _nuxt) {
+  async setup(_options, _nuxt) {
     const resolver = createResolver(import.meta.url)
+    
+    await addComponentsDir({
+      path: resolver.resolve("./runtime/components"), // path of components
+      pathPrefix: false, // Prefix component name by its path.
+      prefix: "", // Prefix all matched components.
+      global: false, // Registers components to be globally available.
+    })
   },
 })
